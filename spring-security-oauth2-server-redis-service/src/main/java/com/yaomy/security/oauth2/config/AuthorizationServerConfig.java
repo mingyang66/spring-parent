7ef849397b600ed0d428bed6e8ff8edc06ca8ee3
yaomingyang <mingyang@eastmoney.com>
2019-07-16 20:32:38 +0800

    'token信息存入redis缓存'

--- spring-security-oauth2-server-redis-service/src/main/java/com/yaomy/security/oauth2/config/AuthorizationServerConfig.java
+++ spring-security-oauth2-server-redis-service/src/main/java/com/yaomy/security/oauth2/config/AuthorizationServerConfig.java
@@ -6,18 +6,17 @@ import com.yaomy.security.oauth2.service.OAuth2ClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * @Description: @EnableAuthorizationServer注解开启OAuth2授权服务机制
@@ -36,6 +35,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private OAuth2ClientDetailsService oAuth2ClientDetailsService;
     @Autowired
     private AuthUserDetailsService authUserDetailsService;
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
     /**
      用来配置客户端详情服务（ClientDetailsService），客户端详情信息在这里初始化，
      你可以把客户端详情信息写死也可以写入内存或者数据库中
@@ -105,7 +106,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         //token保存在内存中（也可以保存在数据库、Redis中）。
         //如果保存在中间件（数据库、Redis），那么资源服务器与认证服务器可以不在同一个工程中。
         //注意：如果不保存access_token，则没法通过access_token取得用户信息
-        return new InMemoryTokenStore();
+        //return new InMemoryTokenStore();
+        return new RedisTokenStore(redisConnectionFactory);
     }
     /**
      * @Description ApprovalStore用户保存、检索和撤销用户审批的界面

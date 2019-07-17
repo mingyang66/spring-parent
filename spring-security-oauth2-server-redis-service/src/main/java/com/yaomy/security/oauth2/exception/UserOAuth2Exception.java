3e0cca1a1f19266b63f160e51e3d8df370ffb78b
yaomingyang <mingyang@eastmoney.com>
2019-07-17 18:28:56 +0800

    '自定义异常处理类优化'

--- spring-security-oauth2-server-redis-service/src/main/java/com/yaomy/security/oauth2/exception/UserOAuth2Exception.java
+++ spring-security-oauth2-server-redis-service/src/main/java/com/yaomy/security/oauth2/exception/UserOAuth2Exception.java
@@ -13,12 +13,19 @@ import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
  */
 @JsonSerialize(using = UserOAuth2ExceptionSerializer.class)
 public class UserOAuth2Exception extends OAuth2Exception {
+    private Integer status = 400;
+
     public UserOAuth2Exception(String message, Throwable t) {
         super(message, t);
+        status = ((OAuth2Exception)t).getHttpErrorCode();
     }
 
     public UserOAuth2Exception(String message) {
         super(message);
+    }
+    @Override
+    public int getHttpErrorCode() {
+        return status;
     }
 
 }

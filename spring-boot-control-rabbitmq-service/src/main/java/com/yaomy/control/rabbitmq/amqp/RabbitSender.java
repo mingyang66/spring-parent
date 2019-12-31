eb10d52a117738a9223e39671431eedeeb882708
yaomingyang <mingyang@eastmoney.com>
2019-12-31 15:37:37 +0800

    '新增消息封装后、消息发送前的扩展点后置处理器'

--- spring-boot-control-rabbitmq-service/src/main/java/com/yaomy/control/rabbitmq/amqp/RabbitSender.java
+++ spring-boot-control-rabbitmq-service/src/main/java/com/yaomy/control/rabbitmq/amqp/RabbitSender.java
@@ -74,7 +74,34 @@ public class RabbitSender {
             }
         }
     };
+    /**
+     * 扩展点，在消息转换完成之后，发送之前调用；可以修改消息属性、消息头信息
+     */
+    private final MessagePostProcessor messagePostProcessor = new MessagePostProcessor() {
+        @Override
+        public Message postProcessMessage(Message message) throws AmqpException {
+            MessageProperties msgProperties = message.getMessageProperties();
+            /**
+             * 设置消息发送到队列之后多久被丢弃，单位：毫秒
+             * 此种方案需要每条消息都设置此属性，比较灵活；
+             * 还有一种方案是在声明队列的时候指定发送到队列中的过期时间；
+             * * Queue queue = new Queue("test_queue2");
+             * * queue.getArguments().put("x-message-ttl", 10000);
+             * 这两种方案可以同时存在，以值小的为准
+             */
+            //msgProperties.setExpiration("10000");
+            /**
+             * 设置消息的优先级
+             */
+            msgProperties.setPriority(9);
+            /**
+             * 设置消息发送到队列中的模式，持久化|非持久化（只存在于内存中）
+             */
+            msgProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
 
+            return message;
+        }
+    };
     /**
      * 发送消息
      * @param exchange 交换器
@@ -104,6 +131,10 @@ public class RabbitSender {
              */
             this.rabbitTemplate.setReturnCallback(returnCallback);
             /**
+             * 新增消息转换完成后、发送之前的扩展点
+             */
+            this.rabbitTemplate.setBeforePublishPostProcessors(messagePostProcessor);
+            /**
              * 将消息主题和属性封装在Message类中
              */
             Message returnedMessage = MessageBuilder.withBody(message.getBytes()).build();
@@ -123,33 +154,7 @@ public class RabbitSender {
             /**
              * 如果msg是org.springframework.amqp.core.Message对象的实例，则直接返回，否则转化为Message对象
              */
-            this.rabbitTemplate.convertAndSend(exchange, routingKey, msg, new MessagePostProcessor() {
-                /**
-                 * 消息后置处理器，消息在转换成Message对象之后调用，可以用来修改消息中的属性、header
-                 */
-                @Override
-                public Message postProcessMessage(Message message) throws AmqpException {
-                    MessageProperties msgProperties = message.getMessageProperties();
-                    /**
-                     * 设置消息发送到队列之后多久被丢弃，单位：毫秒
-                     * 此种方案需要每条消息都设置此属性，比较灵活；
-                     * 还有一种方案是在声明队列的时候指定发送到队列中的过期时间；
-                     * * Queue queue = new Queue("test_queue2");
-                     * * queue.getArguments().put("x-message-ttl", 10000);
-                     * 这两种方案可以同时存在，以值小的为准
-                     */
-                    //msgProperties.setExpiration("10000");
-                    /**
-                     * 设置消息的优先级
-                     */
-                    msgProperties.setPriority(9);
-                    /**
-                     * 设置消息发送到队列中的模式，持久化|非持久化（只存在于内存中）
-                     */
-                    msgProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
-                    return message;
-                }
-            }, correlationData);
+            this.rabbitTemplate.convertAndSend(exchange, routingKey, msg, correlationData);
         } catch (Exception e){
             e.printStackTrace();
         }

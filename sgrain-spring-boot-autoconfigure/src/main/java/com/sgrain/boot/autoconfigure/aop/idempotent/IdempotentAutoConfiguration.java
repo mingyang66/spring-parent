12b0311c3a3c27d6902bfc23b12eecb87e2d3901
mingyang <mingyang@eastmoney.com>
2020-05-19 16:05:32 +0800

    优化生成token接口跟随组件自动的启动

--- sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/aop/idempotent/IdempotentAutoConfiguration.java
+++ sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/aop/idempotent/IdempotentAutoConfiguration.java
@@ -2,14 +2,22 @@ package com.sgrain.boot.autoconfigure.aop.idempotent;
 
 import com.sgrain.boot.autoconfigure.aop.interceptor.IdempotentMethodInterceptor;
 import com.sgrain.boot.common.enums.AopOrderEnum;
+import com.sgrain.boot.common.utils.CharacterUtils;
+import com.sgrain.boot.common.utils.TokenUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @program: spring-parent
@@ -19,17 +27,20 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 @EnableConfigurationProperties(IdempotentProperties.class)
 @ConditionalOnProperty(prefix = "spring.sgrain.idempotent", name = "enable", havingValue = "true", matchIfMissing = true)
+@RestController
 public class IdempotentAutoConfiguration {
     /**
      * 在多个表达式之间使用  || , or 表示  或 ，使用  && , and 表示  与 ， ！ 表示 非
      */
     private static final String REPEAT_SUBMIT_POINT_CUT = StringUtils.join("@annotation(com.sgrain.boot.autoconfigure.aop.annotation.Idempotent)");
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 控制器AOP拦截处理
      */
     @Bean
-    public DefaultPointcutAdvisor repeatSubmitPointCutAdvice(RedisTemplate redisTemplate) {
+    public DefaultPointcutAdvisor repeatSubmitPointCutAdvice() {
         //声明一个AspectJ切点
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         //设置切点表达式
@@ -44,5 +55,14 @@ public class IdempotentAutoConfiguration {
         advisor.setOrder(AopOrderEnum.IDEMPOTENT.getOrder());
 
         return advisor;
+    }
+    /**
+     * 自动生成token令牌，并将令牌存入缓存，过期时间是30s
+     */
+    @GetMapping("api/token/generation")
+    public ResponseEntity<String> generationToken(){
+        String token = TokenUtils.generation();
+        redisTemplate.opsForValue().set(StringUtils.join("idempotent", CharacterUtils.COLON_EN, token), token, 30, TimeUnit.SECONDS);
+        return ResponseEntity.ok(token);
     }
 }

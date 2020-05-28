6ea3895712904d176e214abf652b9bfd4488237e
mingyang <mingyang@eastmoney.com>
2020-05-28 17:33:59 +0800

    新增路径配置文件

--- sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/web/WebAutoConfiguration.java
+++ sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/web/WebAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.sgrain.boot.autoconfigure.web;
 
 import com.sgrain.boot.common.utils.CharacterUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.AntPathMatcher;
@@ -15,7 +17,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @create: 2020/05/26
  */
 @Configuration
+@EnableConfigurationProperties(WebProperties.class)
 public class WebAutoConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private WebProperties webProperties;
+
     /**
      * 配置路由规则
      *
@@ -25,19 +32,19 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer) {
         AntPathMatcher matcher = new AntPathMatcher();
         //区分大小写,默认true
-        matcher.setCaseSensitive(true);
+        matcher.setCaseSensitive(webProperties.getPath().isCaseSensitive());
         //是否去除前后空格,默认false
-        matcher.setTrimTokens(true);
+        matcher.setTrimTokens(webProperties.getPath().isTrimTokens());
         //分隔符
         matcher.setPathSeparator(CharacterUtils.PATH_SEPARATOR);
         //是否缓存匹配规则,默认null等于true
-        matcher.setCachePatterns(true);
+        matcher.setCachePatterns(webProperties.getPath().isCachePatterns());
         //设置路由匹配规则
         configurer.setPathMatcher(matcher);
         //设置URL末尾是否支持斜杠，默认true,如/a/b/有效，/a/b也有效
-        configurer.setUseTrailingSlashMatch(true);
+        configurer.setUseTrailingSlashMatch(webProperties.getPath().isUseTrailingSlashMatch());
         //给所有的接口统一添加前缀
-        configurer.addPathPrefix("api", c -> {
+        configurer.addPathPrefix(webProperties.getPath().getPrefix(), c -> {
             if (c.isAnnotationPresent(RestController.class) || c.isAnnotationPresent(Controller.class)) {
                 return true;
             }
---
+++ sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/web/WebProperties.java
@@ -0,0 +1,75 @@
+package com.sgrain.boot.autoconfigure.web;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @program: spring-parent
+ * @description: Web配置文件
+ * @author: 姚明洋
+ * @create: 2020/05/28
+ */
+@ConfigurationProperties(prefix = "spring.sgrain.web")
+public class WebProperties {
+    private Path path = new Path();
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+
+    public static class Path {
+        //区分大小写
+        private boolean caseSensitive = true;
+        //是否去除前后空格
+        private boolean trimTokens = false;
+        //是否缓存匹配规则,默认null等于true
+        private boolean cachePatterns = true;
+        //设置URL末尾是否支持斜杠，默认true,如/a/b/有效，/a/b也有效
+        private boolean useTrailingSlashMatch = true;
+        //URL默认添加前缀
+        private String prefix = "api";
+
+        public boolean isCaseSensitive() {
+            return caseSensitive;
+        }
+
+        public void setCaseSensitive(boolean caseSensitive) {
+            this.caseSensitive = caseSensitive;
+        }
+
+        public boolean isTrimTokens() {
+            return trimTokens;
+        }
+
+        public void setTrimTokens(boolean trimTokens) {
+            this.trimTokens = trimTokens;
+        }
+
+        public boolean isCachePatterns() {
+            return cachePatterns;
+        }
+
+        public void setCachePatterns(boolean cachePatterns) {
+            this.cachePatterns = cachePatterns;
+        }
+
+        public boolean isUseTrailingSlashMatch() {
+            return useTrailingSlashMatch;
+        }
+
+        public void setUseTrailingSlashMatch(boolean useTrailingSlashMatch) {
+            this.useTrailingSlashMatch = useTrailingSlashMatch;
+        }
+
+        public String getPrefix() {
+            return prefix;
+        }
+
+        public void setPrefix(String prefix) {
+            this.prefix = prefix;
+        }
+    }
+}

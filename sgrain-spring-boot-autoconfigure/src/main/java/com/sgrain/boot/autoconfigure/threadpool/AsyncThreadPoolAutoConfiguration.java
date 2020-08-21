f610f8b1a6c873b758f279666129fd4745177a59
yaomingyang <mingyang@eastmoney.com>
2020-08-21 19:52:58 +0800

    新增异步线程池组件

--- sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/threadpool/AsyncThreadPoolAutoConfiguration.java
+++ sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/threadpool/AsyncThreadPoolAutoConfiguration.java
@@ -1,16 +1,18 @@
-package com.yaomy.control.test.config;
+package com.sgrain.boot.autoconfigure.threadpool;
 
 import com.sgrain.boot.common.utils.LoggerUtils;
 import com.sgrain.boot.common.utils.constant.CharacterUtils;
-import com.sgrain.boot.common.utils.json.JSONUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.lang.reflect.Method;
+import java.util.Objects;
 import java.util.concurrent.Executor;
 
 /**
@@ -20,7 +22,12 @@ import java.util.concurrent.Executor;
  */
 @EnableAsync
 @Configuration
-public class AsyncThreadPoolConfigurer implements AsyncConfigurer {
+@EnableConfigurationProperties(AsyncThreadPoolProperties.class)
+@ConditionalOnProperty(prefix = "spring.sgrain.async-thread-pool", name = "enable", havingValue = "true", matchIfMissing = false)
+public class AsyncThreadPoolAutoConfiguration implements AsyncConfigurer {
+
+    @Autowired
+    private AsyncThreadPoolProperties asyncThreadPoolProperties;
     /**
      * 定义线程池
      *
@@ -33,11 +40,11 @@ public class AsyncThreadPoolConfigurer implements AsyncConfigurer {
         //定义线程池
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         //核心线程数
-        taskExecutor.setCorePoolSize(processors);
+        taskExecutor.setCorePoolSize(Objects.nonNull(asyncThreadPoolProperties.getCorePoolSize()) ? asyncThreadPoolProperties.getCorePoolSize() : processors);
         //线程池最大线程数
-        taskExecutor.setMaxPoolSize(processors * 10);
+        taskExecutor.setMaxPoolSize(Objects.nonNull(asyncThreadPoolProperties.getMaxPoolSize()) ? asyncThreadPoolProperties.getMaxPoolSize() : processors*100);
         //线程队列最大线程数
-        taskExecutor.setQueueCapacity(processors * 100);
+        taskExecutor.setQueueCapacity(Objects.nonNull(asyncThreadPoolProperties.getMaxPoolSize()) ? asyncThreadPoolProperties.getMaxPoolSize() : processors*1000);
         //线程名称前缀
         taskExecutor.setThreadNamePrefix("Async-ThreadPool-");
         //初始化
@@ -53,7 +60,7 @@ public class AsyncThreadPoolConfigurer implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return (throwable, method, objects) -> {
-            LoggerUtils.error(method.getDeclaringClass(), StringUtils.join( method.getDeclaringClass().getName(), CharacterUtils.POINT_SYMBOL, method.getName(), "()发生异常，参数是：", JSONUtils.toJSONString(objects), ",异常：", throwable));
+            LoggerUtils.error(method.getDeclaringClass(), StringUtils.join( method.getDeclaringClass().getName(), CharacterUtils.POINT_SYMBOL, method.getName(), "()发生异常：", throwable));
         };
     }
 }
---
+++ sgrain-spring-boot-autoconfigure/src/main/java/com/sgrain/boot/autoconfigure/threadpool/AsyncThreadPoolProperties.java
@@ -0,0 +1,60 @@
+package com.sgrain.boot.autoconfigure.threadpool;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @program: spring-parent
+ * @description: 异步线程池配置文件
+ * @create: 2020/08/21
+ */
+@ConfigurationProperties(prefix = "spring.sgrain.async-thread-pool")
+public class AsyncThreadPoolProperties {
+    /**
+     * 是否启动异步线程池，默认 false
+     */
+    private boolean enable;
+    /**
+     * 核心线程数,默认：Java虚拟机可用线程数
+     */
+    private Integer corePoolSize;
+    /**
+     * 线程池最大线程数,默认：Java虚拟机可用线程数*100
+     */
+    private Integer maxPoolSize;
+    /**
+     * 线程队列最大线程数,默认：Java虚拟机可用线程数*1000
+     */
+    private Integer queueCapacity;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public Integer getCorePoolSize() {
+        return corePoolSize;
+    }
+
+    public void setCorePoolSize(Integer corePoolSize) {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public Integer getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(Integer maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public Integer getQueueCapacity() {
+        return queueCapacity;
+    }
+
+    public void setQueueCapacity(Integer queueCapacity) {
+        this.queueCapacity = queueCapacity;
+    }
+}

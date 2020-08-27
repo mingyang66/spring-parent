86a342bf785371eab58df1b4f8db025a109ed704
yaomingyang <mingyang@eastmoney.com>
2020-08-27 15:37:05 +0800

    过滤器代码优化

--- sgrain-spring-boot-web/src/main/java/com/sgrain/boot/web/filter/ChannelFilter.java
+++ sgrain-spring-boot-web/src/main/java/com/sgrain/boot/web/filter/ChannelFilter.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
-* @Description: 拦截所有请求过滤器，并将请求类型是HttpServletRequest类型的请求替换为自定义{@link com.sgrain.boot.common.servlet.RequestWrapper}
-* @create: 2020/8/19
-*/
+ * @Description: 拦截所有请求过滤器，并将请求类型是HttpServletRequest类型的请求替换为自定义{@link com.sgrain.boot.common.servlet.RequestWrapper}
+ * @create: 2020/8/19
+ */
 @Component
 @WebFilter(filterName = "channelFilter", urlPatterns = {"/*"})
 public class ChannelFilter implements Filter {
@@ -22,14 +22,12 @@ public class ChannelFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
         try {
-            ServletRequest requestWrapper = null;
+            ServletRequest requestWrapper;
             if (request instanceof HttpServletRequest) {
                 requestWrapper = new RequestWrapper((HttpServletRequest) request);
-            }
-            if (requestWrapper == null) {
-                chain.doFilter(request, response);
-            } else {
                 chain.doFilter(requestWrapper, response);
+            } else {
+                chain.doFilter(request, response);
             }
         } catch (IOException e) {
             e.printStackTrace();

d141655909883d30112c1687cce5b88a3587b5e9
yaomingyang <mingyang@eastmoney.com>
2020-09-07 15:56:39 +0800

    调度打印输入添加日期

--- sgrain-spring-boot-quartz/src/main/java/com/sgrain/boot/quartz/job/CronJob.java
+++ sgrain-spring-boot-quartz/src/main/java/com/sgrain/boot/quartz/job/CronJob.java
@@ -1,9 +1,13 @@
 package com.sgrain.boot.quartz.job;
 
+import com.sgrain.boot.common.enums.DateFormatEnum;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.Trigger;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
 
 /**
  * @program: spring-parent
@@ -16,6 +20,6 @@ public class CronJob extends QuartzJobBean {
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         Trigger trigger = context.getTrigger();
         String url = context.getTrigger().getJobDataMap().getString("url");
-        System.out.println("接收到的地址是："+url);
+        System.out.println(DateFormatUtils.format(new Date(), DateFormatEnum.YYYY_MM_DD_HH_MM_SS.getFormat()) +"接收到的地址是："+url);
     }
 }

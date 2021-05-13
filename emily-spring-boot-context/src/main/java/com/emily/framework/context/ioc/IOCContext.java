9deff4daaff516b84966b1ae8544e9ae28a67cce
yaomingyang <mingyang@eastmoney.com>
2021-05-13 15:50:21 +0800

    IOCContext容器上下文优化调整

--- emily-spring-boot-context/src/main/java/com/emily/framework/context/ioc/IOCContext.java
+++ emily-spring-boot-context/src/main/java/com/emily/framework/context/ioc/IOCContext.java
@@ -27,8 +27,9 @@ public class IOCContext {
 
     /**
      * 获取指定Class类型的实例对象
+     *
      * @param requiredType 实例类型
-     * @param <T> 泛型类类型
+     * @param <T>          泛型类类型
      * @return
      */
     public static <T> T getBean(Class<T> requiredType) {
@@ -37,6 +38,7 @@ public class IOCContext {
 
     /**
      * 获取指定bean实例名称的实例对象
+     *
      * @param name
      * @return
      * @throws BeansException
@@ -47,26 +49,36 @@ public class IOCContext {
 
     /**
      * 获取指定beanname的实例对象，并且转换为指定类型
-     * @param name bean实例名称
+     *
+     * @param name         bean实例名称
      * @param requiredType 目标类型
-     * @param <T> 类型
+     * @param <T>          类型
      * @return
      */
-    public static <T> T getBean(String name, Class<T> requiredType){
+    public static <T> T getBean(String name, Class<T> requiredType) {
         return CONTEXT.getBean(name, requiredType);
     }
+
     /**
-     *
-     * @param name
-     * @param args
+     * @Desc 实例对象必须是原型模式
+     * @param name bean的名称
+     * @param args 实例参数
      * @return
+     * @Bean
+     * @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+     * public Job job(Long id){
+     * Job job = new Job();
+     * job.setId(id);
+     * return job;
+     * }
      */
-    //TODO 待研究
-    public static Object getBean(String name, Object... args){
+    public static Object getBean(String name, Object... args) {
         return CONTEXT.getBean(name, args);
     }
+
     /**
      * 获取指定注解标注的实例bean集合
+     *
      * @param annotationType 注解类型
      * @return
      */
@@ -76,23 +88,27 @@ public class IOCContext {
 
     /**
      * 获取指定注解类型标注的bean实例名称
+     *
      * @param annotationType 主机类型
      * @return
      */
-    public static String[] getBeanNamesForAnnotation(Class<? extends Annotation> annotationType){
+    public static String[] getBeanNamesForAnnotation(Class<? extends Annotation> annotationType) {
         return CONTEXT.getBeanNamesForAnnotation(annotationType);
     }
+
     /**
      * 判定IOC容器中是否存在指定名称的实例bean
+     *
      * @param name bean实例名称
      * @return true|false
      */
-    public static boolean containsBean(String name){
+    public static boolean containsBean(String name) {
         return CONTEXT.containsBean(name);
     }
 
     /**
      * 判定指定bean 名称的实例是否是原型模式
+     *
      * @param name bean实例名称
      * @return true|false
      */
@@ -102,20 +118,22 @@ public class IOCContext {
 
     /**
      * 判定指定的bean实例是否是单例模式
+     *
      * @param name bean实例名称
      * @return true|false
      */
-    public static boolean isSingleton(String name){
+    public static boolean isSingleton(String name) {
         return CONTEXT.isSingleton(name);
     }
 
     /**
      * 判定指定bean名称是否和指定的类型匹配
-     * @param name bean实例名称
+     *
+     * @param name        bean实例名称
      * @param typeToMatch 匹配的类类型
      * @return
      */
-    public static boolean isTypeMatch(String name, Class<?> typeToMatch){
+    public static boolean isTypeMatch(String name, Class<?> typeToMatch) {
         return CONTEXT.isTypeMatch(name, typeToMatch);
     }
 }

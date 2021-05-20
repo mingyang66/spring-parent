b043df92f16ebd8386435b4ece186398a8c1efa0
yaomingyang <mingyang@eastmoney.com>
2021-05-20 19:54:39 +0800

    优化多数据源配置类

--- emily-spring-boot-datasource/src/main/java/com/emily/framework/datasource/context/DataSourceContextHolder.java
+++ emily-spring-boot-datasource/src/main/java/com/emily/framework/datasource/context/DataSourceContextHolder.java
@@ -14,7 +14,7 @@ public class DataSourceContextHolder {
     /**
      * 设置当前线程持有的数据源
      */
-    public static void changeDataSource(String dataSource) {
+    public static void setDataSource(String dataSource) {
         CONTEXT.set(dataSource);
     }
 

4132e5ab05f8273ee4620caf91f1ff25d5e90850
yaomingyang <mingyang@eastmoney.com>
2021-07-16 20:29:29 +0800

    发布新版本3.0.0

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-mvn versions:set -DnewVersion=2.4.8
+mvn versions:set -DnewVersion=3.0.0
 echo '修改版本号'
 mvn versions:commit
 echo '提交修改'
@@ -10,3 +10,6 @@ echo '#########emily-spring-cloud-starter打包完成...'
 sleep 2
 mvn clean install -pl emily-spring-boot-datasource -am
 echo '#########emily-spring-boot-datasource打包完成...'
+sleep 2
+mvn clean install -pl emily-spring-boot-redis -am
+echo '#########emily-spring-boot-redis打包完成...'

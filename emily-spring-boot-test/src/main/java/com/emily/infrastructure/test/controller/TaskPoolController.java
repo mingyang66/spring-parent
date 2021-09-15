57d7b2a9e0fb330457a258721de063037f567825
yaomingyang <mingyang@eastmoney.com>
2021-09-15 09:16:55 +0800

    发布新版本3.9.3

--- emily-spring-boot-test/src/main/java/com/emily/infrastructure/test/controller/TaskPoolController.java
+++ emily-spring-boot-test/src/main/java/com/emily/infrastructure/test/controller/TaskPoolController.java
@@ -1,6 +1,9 @@
 package com.emily.infrastructure.test.controller;
 
 import com.emily.infrastructure.context.helper.ThreadPoolHelper;
+import com.emily.infrastructure.context.ioc.IOCContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,11 @@ public class TaskPoolController {
     @GetMapping("test")
     public void test(){
         ThreadPoolHelper.threadPoolTaskExecutor().submit(() -> System.out.println("------runing..."));
+    }
+
+    @GetMapping("test1")
+    public void test1(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = IOCContext.getBean(ThreadPoolTaskScheduler.class);
+        System.out.println(threadPoolTaskScheduler);
     }
 }

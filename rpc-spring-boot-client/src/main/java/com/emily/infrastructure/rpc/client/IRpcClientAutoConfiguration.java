0f4793ed9273a20a7656ffb774617fc991913278
yaomingyang <mingyang@eastmoney.com>
2021-10-16 15:38:27 +0800

    优化并发布3.10.2版本

--- rpc-spring-boot-client/src/main/java/com/emily/infrastructure/rpc/client/IRpcClientAutoConfiguration.java
+++ rpc-spring-boot-client/src/main/java/com/emily/infrastructure/rpc/client/IRpcClientAutoConfiguration.java
@@ -4,6 +4,10 @@ import com.emily.infrastructure.rpc.client.pool.IRpcConnection;
 import com.emily.infrastructure.rpc.client.pool.IRpcObjectPool;
 import com.emily.infrastructure.rpc.client.pool.IRpcPooledObjectFactory;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -24,14 +28,16 @@ import javax.annotation.PreDestroy;
 @Configuration
 @EnableConfigurationProperties(IRpcClientProperties.class)
 @ConditionalOnProperty(prefix = IRpcClientProperties.PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
-public class IRpcClientAutoConfiguration {
+public class IRpcClientAutoConfiguration implements InitializingBean, DisposableBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(IRpcClientAutoConfiguration.class);
 
     private IRpcObjectPool pool;
 
     @ConditionalOnClass({IRpcPooledObjectFactory.class})
     @Bean
     protected IRpcObjectPool javaObjectPool(IRpcClientProperties properties) {
-        IRpcPooledObjectFactory faceSDKFactory = new IRpcPooledObjectFactory(properties);
+        IRpcPooledObjectFactory factory = new IRpcPooledObjectFactory(properties);
         //设置对象池的相关参数
         GenericObjectPoolConfig<IRpcConnection> poolConfig = new GenericObjectPoolConfig<>();
         poolConfig.setMaxIdle(properties.getPool().getMaxIdle());
@@ -46,7 +52,7 @@ public class IRpcClientAutoConfiguration {
         poolConfig.setJmxEnabled(false);
 
         //新建一个对象池,传入对象工厂和配置
-        pool = new IRpcObjectPool(faceSDKFactory, poolConfig);
+        pool = new IRpcObjectPool(factory, poolConfig);
 
         initPool(properties.getPool().getInitialSize(), properties.getPool().getMaxIdle());
         return pool;
@@ -73,11 +79,16 @@ public class IRpcClientAutoConfiguration {
         }
     }
 
-    @PreDestroy
+    @Override
     public void destroy() {
         if (pool != null) {
             pool.close();
         }
+        logger.info("<== 【销毁--自动化配置】----Rpc客户端销毁成功【IRpcClientAutoConfiguration】");
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("==> 【初始化--自动化配置】----Rpc客户端启动成功【IRpcClientAutoConfiguration】");
+    }
 }

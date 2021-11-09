c8040564958493da8487572d5f28390ce5698ee3
yaomingyang <mingyang@eastmoney.com>
2021-11-09 14:01:29 +0800

    DB多数据源组件记录日志优化

--- emily-spring-boot-datasource/src/main/java/com/emily/infrastructure/mybatis/interceptor/MybatisMethodInterceptor.java
+++ emily-spring-boot-datasource/src/main/java/com/emily/infrastructure/mybatis/interceptor/MybatisMethodInterceptor.java
@@ -1,6 +1,11 @@
 package com.emily.infrastructure.mybatis.interceptor;
 
+import com.emily.infrastructure.common.enums.DateFormatEnum;
 import com.emily.infrastructure.common.exception.PrintExceptionInfo;
+import com.emily.infrastructure.common.utils.json.JSONUtils;
+import com.emily.infrastructure.core.entity.BaseLogger;
+import com.emily.infrastructure.core.helper.ThreadPoolHelper;
+import com.emily.infrastructure.core.holder.ContextHolder;
 import com.google.common.collect.Maps;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
@@ -10,6 +15,8 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,27 +31,46 @@ public class MybatisMethodInterceptor implements MethodInterceptor {
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
+        Method method = invocation.getMethod();
+        boolean isMapper = method.getDeclaringClass().isAnnotationPresent(Mapper.class);
+        long start = System.currentTimeMillis();
+        String action = null;
+        Map<String, Object> paramsMap = null;
+        Object response = null;
         try {
-            Method method = invocation.getMethod();
-            boolean isMapper = method.getDeclaringClass().isAnnotationPresent(Mapper.class);
-            String action = null;
-            Map<String, Object> paramsMap = null;
+            isMapper = method.getDeclaringClass().isAnnotationPresent(Mapper.class);
             if (isMapper) {
-                action = method.getDeclaringClass().getCanonicalName()+"."+method.getName();
+                action = method.getDeclaringClass().getCanonicalName() + "." + method.getName();
                 paramsMap = getInParam(invocation);
             }
-            Object response = invocation.proceed();
-            if (isMapper) {
-                recordLog(action, paramsMap, response);
-            }
+            response = invocation.proceed();
             return response;
         } catch (Throwable ex) {
+            response = PrintExceptionInfo.printErrorInfo(ex);
             throw ex;
+        } finally {
+            if (isMapper) {
+                recordLog(action, paramsMap, response, start);
+            }
         }
     }
 
-    public void recordLog(String action, Map<String, Object> requestParam, Object response) {
-
+    public void recordLog(String action, Map<String, Object> requestParam, Object response, long start) {
+        try {
+            BaseLogger baseLogger = new BaseLogger();
+            baseLogger.setTraceId(ContextHolder.get().getTraceId());
+            baseLogger.setRequestParams(requestParam);
+            baseLogger.setBody(response);
+            baseLogger.setUrl(action);
+            baseLogger.setTriggerTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern(DateFormatEnum.YYYY_MM_DDTHH_MM_SS_COLON_SSS.getFormat())));
+            baseLogger.setTime(System.currentTimeMillis() - start);
+            baseLogger.setMethod("DB");
+            ThreadPoolHelper.threadPoolTaskExecutor().submit(() -> {
+                logger.info(JSONUtils.toJSONString(baseLogger));
+            });
+        } catch (Exception exception) {
+            logger.error(PrintExceptionInfo.printErrorInfo(exception));
+        }
     }
 
     public Map<String, Object> getOutParam(Object outParams) {

90f4c57f313126bd2af3c474d6a9e1dc6ed94fba
yaomingyang <mingyang@eastmoney.com>
2021-12-28 18:21:17 +0800

    优化异步属性配置

--- emily-spring-boot-logback/src/main/java/com/emily/infrastructure/logback/LogbackProperties.java
+++ emily-spring-boot-logback/src/main/java/com/emily/infrastructure/logback/LogbackProperties.java
@@ -39,29 +39,6 @@ public class LogbackProperties {
      */
     private String totalSizeCap = "5GB";
     /**
-     * 是否启用日志异步记录Appender
-     */
-    private boolean asyncAppender;
-    /**
-     * 队列的最大容量，默认为 256
-     */
-    private int asyncQueueSize = 256;
-    /**
-     * 默认，当队列还剩余 20% 的容量时，会丢弃级别为 TRACE, DEBUG 与 INFO 的日志，仅仅只保留 WARN 与 ERROR 级别的日志。想要保留所有的事件，可以设置为 0
-     */
-    private int asyncDiscardingThreshold;
-    /**
-     * 根据所引用 appender 队列的深度以及延迟， AsyncAppender 可能会耗费长时间去刷新队列。
-     * 当 LoggerContext 被停止时， AsyncAppender stop 方法会等待工作线程指定的时间来完成。
-     * 使用 maxFlushTime 来指定最大的刷新时间，单位为毫秒。在指定时间内没有被处理完的事件将会被丢弃。这个属性的值的含义与 Thread.join(long)) 相同
-     * 默认是 1000毫秒
-     */
-    private int asyncMaxFlushTime = 1000;
-    /**
-     * 在队列满的时候 appender 会阻塞而不是丢弃信息。设置为 true，appender 不会阻塞你的应用而会将消息丢弃，默认为 false
-     */
-    private boolean asyncNeverBlock;
-    /**
      * 基础根日志
      */
     private Root root = new Root();
@@ -73,6 +50,10 @@ public class LogbackProperties {
      * 按模块记录日志
      */
     private Module module = new Module();
+    /**
+     * 异步日志配置
+     */
+    private Async async = new Async();
 
     public boolean isEnabled() {
         return enabled;
@@ -122,46 +103,6 @@ public class LogbackProperties {
         this.sizeAndTimeRollingPolicy = sizeAndTimeRollingPolicy;
     }
 
-    public boolean isAsyncAppender() {
-        return asyncAppender;
-    }
-
-    public void setAsyncAppender(boolean asyncAppender) {
-        this.asyncAppender = asyncAppender;
-    }
-
-    public int getAsyncQueueSize() {
-        return asyncQueueSize;
-    }
-
-    public void setAsyncQueueSize(int asyncQueueSize) {
-        this.asyncQueueSize = asyncQueueSize;
-    }
-
-    public int getAsyncDiscardingThreshold() {
-        return asyncDiscardingThreshold;
-    }
-
-    public void setAsyncDiscardingThreshold(int asyncDiscardingThreshold) {
-        this.asyncDiscardingThreshold = asyncDiscardingThreshold;
-    }
-
-    public int getAsyncMaxFlushTime() {
-        return asyncMaxFlushTime;
-    }
-
-    public void setAsyncMaxFlushTime(int asyncMaxFlushTime) {
-        this.asyncMaxFlushTime = asyncMaxFlushTime;
-    }
-
-    public boolean isAsyncNeverBlock() {
-        return asyncNeverBlock;
-    }
-
-    public void setAsyncNeverBlock(boolean asyncNeverBlock) {
-        this.asyncNeverBlock = asyncNeverBlock;
-    }
-
     public Root getRoot() {
         return root;
     }
@@ -184,6 +125,14 @@ public class LogbackProperties {
 
     public void setModule(Module module) {
         this.module = module;
+    }
+
+    public Async getAsync() {
+        return async;
+    }
+
+    public void setAsync(Async async) {
+        this.async = async;
     }
 
     /**
@@ -302,6 +251,72 @@ public class LogbackProperties {
 
         public void setConsole(boolean console) {
             this.console = console;
+        }
+    }
+
+    public static class Async {
+        /**
+         * 是否启用日志异步记录Appender
+         */
+        private boolean asyncAppender;
+        /**
+         * 队列的最大容量，默认为 256
+         */
+        private int asyncQueueSize = 256;
+        /**
+         * 默认，当队列还剩余 20% 的容量时，会丢弃级别为 TRACE, DEBUG 与 INFO 的日志，仅仅只保留 WARN 与 ERROR 级别的日志。想要保留所有的事件，可以设置为 0
+         */
+        private int asyncDiscardingThreshold;
+        /**
+         * 根据所引用 appender 队列的深度以及延迟， AsyncAppender 可能会耗费长时间去刷新队列。
+         * 当 LoggerContext 被停止时， AsyncAppender stop 方法会等待工作线程指定的时间来完成。
+         * 使用 maxFlushTime 来指定最大的刷新时间，单位为毫秒。在指定时间内没有被处理完的事件将会被丢弃。这个属性的值的含义与 Thread.join(long)) 相同
+         * 默认是 1000毫秒
+         */
+        private int asyncMaxFlushTime = 1000;
+        /**
+         * 在队列满的时候 appender 会阻塞而不是丢弃信息。设置为 true，appender 不会阻塞你的应用而会将消息丢弃，默认为 false
+         */
+        private boolean asyncNeverBlock;
+
+        public boolean isAsyncAppender() {
+            return asyncAppender;
+        }
+
+        public void setAsyncAppender(boolean asyncAppender) {
+            this.asyncAppender = asyncAppender;
+        }
+
+        public int getAsyncQueueSize() {
+            return asyncQueueSize;
+        }
+
+        public void setAsyncQueueSize(int asyncQueueSize) {
+            this.asyncQueueSize = asyncQueueSize;
+        }
+
+        public int getAsyncDiscardingThreshold() {
+            return asyncDiscardingThreshold;
+        }
+
+        public void setAsyncDiscardingThreshold(int asyncDiscardingThreshold) {
+            this.asyncDiscardingThreshold = asyncDiscardingThreshold;
+        }
+
+        public int getAsyncMaxFlushTime() {
+            return asyncMaxFlushTime;
+        }
+
+        public void setAsyncMaxFlushTime(int asyncMaxFlushTime) {
+            this.asyncMaxFlushTime = asyncMaxFlushTime;
+        }
+
+        public boolean isAsyncNeverBlock() {
+            return asyncNeverBlock;
+        }
+
+        public void setAsyncNeverBlock(boolean asyncNeverBlock) {
+            this.asyncNeverBlock = asyncNeverBlock;
         }
     }
 }

71447d9c563b69b62d9706b9ab7e852859024a48
yaomingyang <mingyang@eastmoney.com>
2022-02-10 17:29:06 +0800

    日志初始化判定逻辑优化

--- emily-spring-boot-logback/src/main/java/com/emily/infrastructure/logback/util/PropertyUtils.java
+++ emily-spring-boot-logback/src/main/java/com/emily/infrastructure/logback/util/PropertyUtils.java
@@ -14,17 +14,17 @@ public abstract class PropertyUtils {
     public static final String BOOTSTRAP_ENABLED_PROPERTY = "spring.cloud.bootstrap.enabled";
     public static final String USE_LEGACY_PROCESSING_PROPERTY = "spring.config.use-legacy-processing";
     public static final String MARKER_CLASS = "org.springframework.cloud.bootstrap.marker.Marker";
-    public static final boolean MARKER_CLASS_EXISTS = ClassUtils.isPresent("org.springframework.cloud.bootstrap.marker.Marker", (ClassLoader)null);
+    public static final boolean MARKER_CLASS_EXISTS = ClassUtils.isPresent(MARKER_CLASS, (ClassLoader) null);
 
     private PropertyUtils() {
         throw new UnsupportedOperationException("unable to instatiate utils class");
     }
 
     public static boolean bootstrapEnabled(Environment environment) {
-        return environment.getProperty("spring.cloud.bootstrap.enabled", Boolean.class, false) || MARKER_CLASS_EXISTS;
+        return environment.getProperty(BOOTSTRAP_ENABLED_PROPERTY, Boolean.class, false) || MARKER_CLASS_EXISTS;
     }
 
     public static boolean useLegacyProcessing(Environment environment) {
-        return environment.getProperty("spring.config.use-legacy-processing", Boolean.class, false);
+        return environment.getProperty(USE_LEGACY_PROCESSING_PROPERTY, Boolean.class, false);
     }
 }

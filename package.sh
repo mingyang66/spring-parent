a0f9716414d297f452110cb289929f44f7985a9b
yaomingyang <mingyang@eastmoney.com>
2023-05-09 13:23:29 +0800

    modify pom file

--- package.sh
+++ package.sh
@@ -1,14 +1,14 @@
-./mvnw versions:set -DnewVersion=4.2.1
+mvn versions:set -DnewVersion=4.2.5
 echo '修改版本号'
-./mvnw versions:commit
+mvn versions:commit
 echo '提交修改'
-./mvnw clean install -pl emily-spring-boot-starter -am
+mvn clean deploy -pl emily-spring-boot-starter -am
 echo '#########emily-spring-boot-starter打包完成...'
-./mvnw clean install -pl emily-spring-cloud-starter -am
+mvn clean deploy -pl emily-spring-cloud-starter -am
 echo '#########emily-spring-cloud-starter打包完成...'
-./mvnw clean install -pl emily-spring-boot-datasource -am
+mvn clean deploy -pl emily-spring-boot-datasource -am
 echo '#########emily-spring-boot-datasource打包完成...'
-./mvnw clean install -pl emily-spring-boot-redis -am
+mvn clean deploy -pl emily-spring-boot-redis -am
 echo '#########emily-spring-boot-redis打包完成...'
-./mvnw clean install -pl emily-spring-boot-rabbitmq -am
+mvn clean deploy -pl emily-spring-boot-rabbitmq -am
 echo '#########emily-spring-boot-rabbitmq打包完成...'

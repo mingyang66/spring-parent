bc7c8b0af21e8c7c408d5af4817731f5be87f9cf
yaomingyang <mingyang@eastmoney.com>
2023-05-12 17:19:29 +0800

    发布拆包后新版本

--- package.sh
+++ package.sh
@@ -1,24 +1,27 @@
-mvn versions:set -DnewVersion=4.3.0.4
+mvn versions:set -DnewVersion=4.3.1
 echo '修改版本号'
 mvn versions:commit
 echo '提交修改'
 mvn clean deploy -pl oceansky-commons -am
 echo '#########emily-spring-boot-common打包完成...'
 mvn clean deploy -pl oceansky-json
+mvn clean deploy -pl oceansky-captcha
+mvn clean deploy -pl oceansky-language
+mvn clean deploy -pl oceansky-sensitive
 echo '#########oceansky-json打包完成...'
 mvn clean deploy -pl emily-spring-boot-parent
 echo '#########emily-spring-boot-parent打包完成...'
 mvn clean deploy -pl emily-spring-boot-logback
 echo '#########emily-spring-boot-logback打包完成...'
-#mvn clean deploy -pl emily-spring-boot-core
+mvn clean deploy -pl emily-spring-boot-core
 echo '#########emily-spring-boot-core打包完成...'
-#mvn clean deploy -pl emily-spring-boot-starter
+mvn clean deploy -pl emily-spring-boot-starter
 echo '#########emily-spring-boot-starter打包完成...'
-#mvn clean deploy -pl emily-spring-cloud-starter
+mvn clean deploy -pl emily-spring-cloud-starter
 echo '#########emily-spring-cloud-starter打包完成...'
-#mvn clean deploy -pl emily-spring-boot-datasource
+mvn clean deploy -pl emily-spring-boot-datasource
 echo '#########emily-spring-boot-datasource打包完成...'
-#mvn clean deploy -pl emily-spring-boot-redis
+mvn clean deploy -pl emily-spring-boot-redis
 echo '#########emily-spring-boot-redis打包完成...'
-#mvn clean deploy -pl emily-spring-boot-rabbitmq
+mvn clean deploy -pl emily-spring-boot-rabbitmq
 echo '#########emily-spring-boot-rabbitmq打包完成...'

83d007d1e77d5c29fa10e9d16b9fd70b7cce454c
yaomingyang <mingyang@eastmoney.com>
2023-05-31 09:25:34 +0800

    新增单元测试类

---
+++ oceansky-json/src/test/java/com/emily/infrastructure/json/test/JsonObjectTest.java
@@ -0,0 +1,15 @@
+package com.emily.infrastructure.json.test;
+
+import org.junit.Test;
+
+/**
+ * @Description :  json对象单元测试
+ * @Author :  Emily
+ * @CreateDate :  Created in 2023/5/28 7:26 PM
+ */
+public class JsonObjectTest {
+    @Test
+    public void test(){
+
+    }
+}

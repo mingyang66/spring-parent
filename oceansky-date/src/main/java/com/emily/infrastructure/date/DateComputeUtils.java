3baefba3a637ac90b36e463f1237f1c7b1aeb885
yaomingyang <mingyang@eastmoney.com>
2023-05-31 10:53:38 +0800

    新增判定是否闰年方法isleapYear

--- oceansky-date/src/main/java/com/emily/infrastructure/date/DateComputeUtils.java
+++ oceansky-date/src/main/java/com/emily/infrastructure/date/DateComputeUtils.java
@@ -187,9 +187,7 @@ public class DateComputeUtils {
     public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                }
+                return year % 400 == 0;
             } else {
                 return true;
             }

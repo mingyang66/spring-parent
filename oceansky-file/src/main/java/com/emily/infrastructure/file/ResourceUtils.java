fba657cd837a9f0cf4c45ce8f5f67f4d92968848
yaomingyang <mingyang@eastmoney.com>
2023-06-01 15:20:55 +0800

    重命名ResourceUtils组件

--- oceansky-file/src/main/java/com/emily/infrastructure/file/ResourceUtils.java
+++ oceansky-file/src/main/java/com/emily/infrastructure/file/ResourceUtils.java
@@ -76,9 +76,6 @@ public class ResourceUtils {
                 properties.load(inStream);
             }
             return properties;
-        } catch (Exception ex) {
-            System.out.println("load config file error, file : " + filePath);
-            throw ex;
         } finally {
             if (inStream != null) {
                 inStream.close();

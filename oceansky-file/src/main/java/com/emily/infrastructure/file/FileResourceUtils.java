60292999e1e16a949d7a187e412f7f1c9212862b
yaomingyang <mingyang@eastmoney.com>
2023-06-01 10:30:26 +0800

    优化file组件

--- oceansky-file/src/main/java/com/emily/infrastructure/file/FileResourceUtils.java
+++ oceansky-file/src/main/java/com/emily/infrastructure/file/FileResourceUtils.java
@@ -17,18 +17,33 @@ public class FileResourceUtils {
     public static final String XML_SUFFIX = ".xml";
 
     /**
+     * 读取指定路径下的问题件，如果读取异常则置为null
+     *
+     * @param filePath 文件路径
+     * @return 键值对类型对象
+     */
+    public static Properties loadConfigElseGet(String filePath) {
+        try {
+            return loadConfig(filePath);
+        } catch (IOException e) {
+            System.out.println("load config file error" + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
      * 读取指定路径下的文件，并将文件转换为key-value类型
      * -----------------------------------------------------------------------
      * 一、读取属性配置文件(properties、yaml类型)：
      * 读取classpath环境变量下的配置：classpath:test.properties
      * 读取绝对路径下的配置：/Users/xx/Documents/IDE/workplace-java/spring-parent/oceansky-file/src/test/resources/test.properties
-     * 读取http获取https远程目录下的配置：http://www.xx.xxx/test.properties
+     * 读取http获取https远程目录下的配置：<a href="http://www.xx.xxx/test.properties">...</a>
      * 读取file路径下配置文件：file://a.ba/test.properties
      * 二、同样支持读取各种路径下的xml属性配置文件
      * -----------------------------------------------------------------------
      *
      * @param filePath 文件路径
-     * @return 键值对类型配置文件
+     * @return 键值对类型对象
      */
     public static Properties loadConfig(String filePath) throws IOException {
         Properties properties = new Properties();
@@ -63,9 +78,11 @@ public class FileResourceUtils {
             return properties;
         } catch (Exception ex) {
             System.out.println("load config file error, file : " + filePath);
-            return null;
+            throw ex;
         } finally {
-            inStream.close();
+            if (inStream != null) {
+                inStream.close();
+            }
         }
     }
 
--- oceansky-file/src/test/java/com/emily/infrastructure/file/test/FileResourceUtilsTest.java
+++ oceansky-file/src/test/java/com/emily/infrastructure/file/test/FileResourceUtilsTest.java
@@ -4,7 +4,6 @@ import com.emily.infrastructure.file.FileResourceUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -14,27 +13,28 @@ import java.util.Properties;
  */
 public class FileResourceUtilsTest {
     @Test
-    public void loadConfig() throws IOException, IllegalAccessException {
+    public void loadConfig() {
         String path = "classpath:test.properties";
-        Properties properties = FileResourceUtils.loadConfig(path);
+        Properties properties = FileResourceUtils.loadConfigElseGet(path);
         Assert.assertEquals(properties.getProperty("test.key"), "testkey");
 
         path = "/Users/yaomingyang/Documents/IDE/workplace-java/spring-parent/oceansky-file/src/test/resources/test.properties";
-        properties = FileResourceUtils.loadConfig(path);
+        properties = FileResourceUtils.loadConfigElseGet(path);
         Assert.assertEquals(properties.getProperty("test.key"), "testkey");
 
         path = "https://www.baidu.com/";
-        properties = FileResourceUtils.loadConfig(path);
+        properties = FileResourceUtils.loadConfigElseGet(path);
         Assert.assertEquals(properties.getProperty("<!DOCTYPE"), "html>");
 
         path = "classpath:test.xml";
-        properties = FileResourceUtils.loadConfig(path);
+        properties = FileResourceUtils.loadConfigElseGet(path);
         Assert.assertEquals(properties.getProperty("username"), "孙少平");
         Assert.assertEquals(properties.getProperty("password"), "123456");
 
         path = "classpath:test.yaml";
-        properties = FileResourceUtils.loadConfig(path);
-        System.out.println(properties);
+        properties = FileResourceUtils.loadConfigElseGet(path);
+        Assert.assertEquals(properties.getProperty("username"), "tiaoxiao");
+        Assert.assertEquals(properties.getProperty("password"), "1234");
 
     }
 }

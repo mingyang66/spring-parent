6ee70d6c4dd591c84582f363c2d17dd4b3d87a25
yaomingyang <mingyang@eastmoney.com>
2023-09-09 13:47:46 +0800

    code optimize

--- emily-spring-boot-rabbitmq/src/main/java/com/emily/infrastructure/rabbitmq/RabbitMqAutoConfiguration.java
+++ emily-spring-boot-rabbitmq/src/main/java/com/emily/infrastructure/rabbitmq/RabbitMqAutoConfiguration.java
@@ -6,6 +6,11 @@ import com.emily.infrastructure.rabbitmq.amqp.RabbitMqMessagingTemplateConfigura
 import com.emily.infrastructure.rabbitmq.amqp.RabbitMqTemplateConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.MultiRabbitBootstrapConfiguration;
+import org.springframework.amqp.rabbit.annotation.RabbitBootstrapConfiguration;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerAnnotationBeanPostProcessor;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurationSelector;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -17,9 +22,15 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Role;
 
 /**
- * rabbitmq自动化配置
+ * RabbitMQ自动化初始配置类
  *
  * @author Emily
+ * @see RabbitListenerConfigurationSelector RabbitMQ监听器@RabbitListener相关类初始化入口
+ * @see MultiRabbitBootstrapConfiguration
+ * @see RabbitBootstrapConfiguration
+ * @see RabbitListenerAnnotationBeanPostProcessor
+ * @see RabbitListenerEndpointRegistry
+ * @see RabbitAutoConfiguration
  * @since Created in 2022/6/2 4:58 下午
  */
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)

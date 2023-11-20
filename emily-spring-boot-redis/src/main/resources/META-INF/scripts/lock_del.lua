fdd0c1866c95eb78e52e5fe67745e941c7200cea
yaomingyang <mingyang@eastmoney.com>
2023-11-20 13:21:29 +0800

    修改lua脚本变量名

--- emily-spring-boot-redis/src/main/resources/META-INF/scripts/lock_del.lua
+++ emily-spring-boot-redis/src/main/resources/META-INF/scripts/lock_del.lua
@@ -3,9 +3,9 @@ local key = KEYS[1]
 -- 锁标识
 local value = ARGV[1]
 -- 获取锁标识
-local v = redis.call('GET', key)
+local marker = redis.call('GET', key)
 -- 如果标识一致，则释放锁；否则标注对应的锁已经释放
-if v == value then
+if marker == value then
     -- 删除已存在的键,不存在的 key 会被忽略
     return redis.call('DEL', key)
 else

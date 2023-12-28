a52f572129c49d1fd708ddc028d6e3e58a0614b5
yaomingyang <mingyang@eastmoney.com>
2023-12-28 17:56:12 +0800

    optimize new add annotation

--- emily-spring-boot-starter/src/main/java/com/emily/infrastructure/autoconfigure/valid/annotation/IsLocalTime.java
+++ emily-spring-boot-starter/src/main/java/com/emily/infrastructure/autoconfigure/valid/annotation/IsLocalTime.java
@@ -8,11 +8,15 @@ import java.lang.annotation.*;
 
 /**
  * 校验是值否为符合指定日期格式要求
+ * 1. ElementType.ANNOTATION_TYPE 用户其它约束的约束注解
+ * 2. ElementType.FIELD 受约束的属性字段
+ * 3. ElementType.PARAMETER 用于受约束的方法和构造函数参数
+ * 4. ElementType.METHOD 用于受约束的getter和受约束的方法返回值
  *
  * @author :  Emily
  * @since :  2023/12/24 1:35 PM
  */
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = {IsLocalTimeValidator.class})

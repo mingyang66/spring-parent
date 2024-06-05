002a107f8c9123fa51aa82d6d4b7c9d138959992
yaomingyang <mingyang@eastmoney.com>
2024-06-05 14:16:44 +0800

    StatusPrinter替换为StatusPrinter2类打印logback内部融资状态信息

--- oceansky-logger/src/main/java/com/emily/infrastructure/logger/configuration/context/ConfigurationAction.java
+++ oceansky-logger/src/main/java/com/emily/infrastructure/logger/configuration/context/ConfigurationAction.java
@@ -5,7 +5,7 @@ import ch.qos.logback.core.spi.ContextAwareBase;
 import ch.qos.logback.core.status.InfoStatus;
 import ch.qos.logback.core.status.OnConsoleStatusListener;
 import ch.qos.logback.core.util.StatusListenerConfigHelper;
-import ch.qos.logback.core.util.StatusPrinter;
+import ch.qos.logback.core.util.StatusPrinter2;
 import com.emily.infrastructure.logger.configuration.property.LoggerConfig;
 
 /**
@@ -21,6 +21,7 @@ import com.emily.infrastructure.logger.configuration.property.LoggerConfig;
  * @since :  Created in 2023/7/15 5:32 PM
  */
 public class ConfigurationAction extends ContextAwareBase {
+    private final static StatusPrinter2 SINGLETON = new StatusPrinter2();
     static final String INTERNAL_DEBUG_ATTR = "debug";
     static final String PACKAGING_DATA_ATTR = "packagingData";
     //static final String SCAN_ATTR = "scan";
@@ -40,7 +41,7 @@ public class ConfigurationAction extends ContextAwareBase {
     public void start() {
         if (Boolean.getBoolean(DEBUG_SYSTEM_PROPERTY_KEY) || Boolean.getBoolean(INTERNAL_DEBUG_ATTR) || config.isDebug()) {
             //是否报告logback内部状态信息
-            StatusPrinter.print(context);
+            SINGLETON.print(context);
             //开启内部debug模式
             StatusListenerConfigHelper.addOnConsoleListenerInstance(context, new OnConsoleStatusListener());
         } else {

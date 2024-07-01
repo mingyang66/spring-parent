4e1fb9c11dc4941de4b4a5cbd4f4fb12f4186d51
yaomingyang <mingyang@eastmoney.com>
2024-07-01 14:11:46 +0800

    redis sdk support FailureAnalyzer

---
+++ emily-spring-boot-redis/src/main/java/com/emily/infrastructure/redis/diagnostics/analyzer/RedisIdentifierFailureAnalyzer.java
@@ -0,0 +1,17 @@
+package com.emily.infrastructure.redis.diagnostics.analyzer;
+
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+/**
+ * Redis默认数据库配置故障分析
+ *
+ * @author :  Emily
+ * @since :  2024/7/1 下午20:46
+ */
+public class RedisIdentifierFailureAnalyzer extends AbstractFailureAnalyzer<NullPointerException> {
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, NullPointerException cause) {
+        return new FailureAnalysis(cause.getMessage(), "Redis默认数据库标识配置缺失", cause);
+    }
+}
---
+++ emily-spring-boot-redis/src/main/java/com/emily/infrastructure/redis/diagnostics/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 错误报告诊断
+ *
+ * @author :  Emily
+ * @since :  2024/7/1 下午20:43
+ */
+package com.emily.infrastructure.redis.diagnostics;

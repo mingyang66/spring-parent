61d9616b16eb0684346176db772354f3069b693d
yaomingyang <mingyang@eastmoney.com>
2024-09-03 20:06:07 +0800

    release new version

--- package.sh
+++ package.sh
@@ -2,32 +2,32 @@
 #echo '修改版本号'
 #./mvnw versions:commit
 #echo '提交修改'
-./mvnw clean install -pl emily-project -am
+./mvnw clean deploy -pl emily-project -am
 echo '#########emily-parent...'
 cd emily-project
-../mvnw clean install -pl oceansky-json
-../mvnw clean install -pl oceansky-logger
-../mvnw clean install -pl oceansky-captcha
-../mvnw clean install -pl oceansky-language
-../mvnw clean install -pl oceansky-sensitive
-../mvnw clean install -pl oceansky-jwt
-../mvnw clean install -pl oceansky-date
-../mvnw clean install -pl oceansky-common
+../mvnw clean deploy -pl oceansky-json
+../mvnw clean deploy -pl oceansky-logger
+../mvnw clean deploy -pl oceansky-captcha
+../mvnw clean deploy -pl oceansky-language
+../mvnw clean deploy -pl oceansky-sensitive
+../mvnw clean deploy -pl oceansky-jwt
+../mvnw clean deploy -pl oceansky-date
+../mvnw clean deploy -pl oceansky-common
 cd ../emily-spring-project
-../mvnw clean install -pl otter-spring-resource -am
-../mvnw clean install -pl otter-spring-servlet
+../mvnw clean deploy -pl otter-spring-resource -am
+../mvnw clean deploy -pl otter-spring-servlet
 cd ../emily-spring-boot-project
 # 独立
-../mvnw clean install -pl emily-spring-boot-aop -am
-../mvnw clean install -pl emily-spring-boot-tracing
-../mvnw clean install -pl emily-spring-boot-logger
-../mvnw clean install -pl emily-spring-boot-redis
-../mvnw clean install -pl emily-spring-boot-validation
+../mvnw clean deploy -pl emily-spring-boot-aop -am
+../mvnw clean deploy -pl emily-spring-boot-tracing
+../mvnw clean deploy -pl emily-spring-boot-logger
+../mvnw clean deploy -pl emily-spring-boot-redis
+../mvnw clean deploy -pl emily-spring-boot-validation
 # 依赖其它
-../mvnw clean install -pl emily-spring-boot-datasource
-../mvnw clean install -pl emily-spring-boot-rabbitmq
-../mvnw clean install -pl emily-spring-boot-starter
-../mvnw clean install -pl emily-spring-boot-transfer
-../mvnw clean install -pl emily-spring-boot-web
+../mvnw clean deploy -pl emily-spring-boot-datasource
+../mvnw clean deploy -pl emily-spring-boot-rabbitmq
+../mvnw clean deploy -pl emily-spring-boot-starter
+../mvnw clean deploy -pl emily-spring-boot-transfer
+../mvnw clean deploy -pl emily-spring-boot-web
 
 echo '#########打包完成...'

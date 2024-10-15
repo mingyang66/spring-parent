5ad637f18432b82fdf53de5005548ea38e0b5ae8
yaomingyang <mingyang@eastmoney.com>
2024-10-15 14:38:24 +0800

    optimize code

--- emily-spring-boot-samples/websocket-spring-boot-sample/src/main/java/com/emily/simple/websocket/config/WebSocketConfig.java
+++ emily-spring-boot-samples/websocket-spring-boot-sample/src/main/java/com/emily/simple/websocket/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 注册STOMP端点，指定端点的路径
-        registry.addEndpoint("/chatroom/{userid}")
+        registry.addEndpoint("/chatroom", "/chatroom/{userid}")
                 // 配置websocket握手请求
                 .setHandshakeHandler(new ChatHandshakeHandler());
     }

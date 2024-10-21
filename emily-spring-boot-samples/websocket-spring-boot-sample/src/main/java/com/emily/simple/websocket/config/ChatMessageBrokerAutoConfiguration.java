447c4a2d679671b4642f297a0308f390e6d8d6e8
yaomingyang <mingyang@eastmoney.com>
2024-10-21 17:14:33 +0800

    optimize heartbeat taskScheduler

--- emily-spring-boot-samples/websocket-spring-boot-sample/src/main/java/com/emily/simple/websocket/config/ChatMessageBrokerAutoConfiguration.java
+++ emily-spring-boot-samples/websocket-spring-boot-sample/src/main/java/com/emily/simple/websocket/config/ChatMessageBrokerAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.emily.simple.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -47,7 +48,7 @@ public class ChatMessageBrokerAutoConfiguration implements WebSocketMessageBroke
         registry.setApplicationDestinationPrefixes("/app");
     }
 
-    protected ThreadPoolTaskScheduler initDefaultHeartbeatTaskScheduler() {
+    protected TaskScheduler initDefaultHeartbeatTaskScheduler() {
         //定义心跳任务调度器
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(Runtime.getRuntime().availableProcessors());

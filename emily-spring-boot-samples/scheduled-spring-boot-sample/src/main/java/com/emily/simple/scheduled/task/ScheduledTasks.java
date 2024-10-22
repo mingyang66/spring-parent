9e635a4343e9bde2d473f8c749b9c3253dc4dc7d
yaomingyang <mingyang@eastmoney.com>
2024-10-22 15:59:27 +0800

    update scheduled cron

--- emily-spring-boot-samples/scheduled-spring-boot-sample/src/main/java/com/emily/simple/scheduled/task/ScheduledTasks.java
+++ emily-spring-boot-samples/scheduled-spring-boot-sample/src/main/java/com/emily/simple/scheduled/task/ScheduledTasks.java
@@ -15,7 +15,7 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "5,15,25,35,45,55 * * * * ?")
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }

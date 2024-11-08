32c6a3e0bbcbee0af05859604bbb614918a48c28
yaomingyang <mingyang@eastmoney.com>
2024-11-08 13:14:45 +0800

    optimize RestTemplate sdk

--- emily-spring-boot-project/emily-spring-boot-transfer/src/main/java/com/emily/infrastructure/transfer/rest/handler/CustomResponseErrorHandler.java
+++ emily-spring-boot-project/emily-spring-boot-transfer/src/main/java/com/emily/infrastructure/transfer/rest/handler/CustomResponseErrorHandler.java
@@ -1,6 +1,8 @@
 package com.emily.infrastructure.transfer.rest.handler;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
@@ -11,20 +13,14 @@ import java.io.IOException;
  * @author Emily
  * @since 2020/08/18
  */
-public class CustomResponseErrorHandler implements ResponseErrorHandler {
+public class CustomResponseErrorHandler extends DefaultResponseErrorHandler {
     /**
      * 判定响应是否有任何错误
      *
-     * @param response 响应对象
      * @return true :返回的响应有错误，false无错误
-     * @throws IOException 异常
      */
-    @Override
-    public boolean hasError(ClientHttpResponse response) throws IOException {
+    protected boolean hasError(HttpStatusCode statusCode) {
         return true;
     }
 
-    @Override
-    public void handleError(ClientHttpResponse response) throws IOException {
-    }
 }

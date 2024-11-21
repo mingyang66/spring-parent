7d06160a4bcf6c74da6bc935be2bfb81b8beef84
yaomingyang <mingyang@eastmoney.com>
2024-11-21 10:10:26 +0800

    optimize web-spring-boot-sample code

---
+++ emily-spring-boot-samples/web-emily-spring-sample/src/main/java/com/emily/infrastructure/sample/web/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Zhejiang xiaominfo Technology CO.,LTD.
+ * All rights reserved.
+ * Official Web Site: http://www.xiaominfo.com.
+ * Developer Web Site: http://open.xiaominfo.com.
+ */
+
+package com.emily.infrastructure.sample.web.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/***
+ * 创建Swagger配置
+ * @since:knife4j-springdoc-openapi-demo 1.0
+ * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a>
+ * 2020/03/15 20:40
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Contact contact = new Contact();
+        contact.setName("老顽童");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("XXX用户系统API")
+                        .version("4.0.0")
+                        .contact(contact)
+                        .description( "Knife4j集成springdoc-openapi示例")
+                        .termsOfService("http://doc.xiaominfo.com")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://doc.xiaominfo.com")));
+    }
+
+
+}

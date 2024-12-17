a09ba35bac413a040ba76dd07b9b19e5a8305357
yaomingyang <mingyang@eastmoney.com>
2024-12-17 09:37:24 +0800

    update i18n interceptor

--- emily-spring-boot-project/emily-spring-boot-i18n/src/main/java/com/emily/infrastructure/i18n/interceptor/DefaultI18nMethodInterceptor.java
+++ emily-spring-boot-project/emily-spring-boot-i18n/src/main/java/com/emily/infrastructure/i18n/interceptor/DefaultI18nMethodInterceptor.java
@@ -2,13 +2,10 @@ package com.emily.infrastructure.i18n.interceptor;
 
 import com.emily.infrastructure.aop.constant.AopOrderInfo;
 import com.emily.infrastructure.common.constant.HeaderInfo;
-import com.emily.infrastructure.i18n.annotation.I18nOperation;
 import com.emily.infrastructure.language.convert.I18nUtils;
 import com.emily.infrastructure.language.convert.LanguageType;
 import com.otter.infrastructure.servlet.RequestUtils;
 import org.aopalliance.intercept.MethodInvocation;
-
-import java.lang.reflect.Method;
 
 /**
  * 多语言拦截器
@@ -19,24 +16,20 @@ import java.lang.reflect.Method;
 public class DefaultI18nMethodInterceptor implements I18nCustomizer {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        Method method = invocation.getMethod();
-        //判断是否需要多语言处理
-        if (method.isAnnotationPresent(I18nOperation.class)) {
-            //执行结果
-            Object response = invocation.proceed();
-            if (response == null) {
-                return null;
-            }
-            //语言类型
-            LanguageType languageType = LanguageType.getByCode(RequestUtils.getHeader(HeaderInfo.LANGUAGE));
-            // 如果是字符串直接转换
-            if (response instanceof String value) {
-                return I18nUtils.doGetProperty(value, languageType);
-            }
-            //将结果翻译为指定语言类型
-            return I18nUtils.translate(response, languageType);
+        //执行结果
+        Object response = invocation.proceed();
+        if (response == null) {
+            return null;
         }
-        return invocation.proceed();
+        //语言类型
+        LanguageType languageType = LanguageType.getByCode(RequestUtils.getHeader(HeaderInfo.LANGUAGE));
+        // 如果是字符串直接转换
+        if (response instanceof String value) {
+            return I18nUtils.doGetProperty(value, languageType);
+        }
+        //将结果翻译为指定语言类型
+        return I18nUtils.translate(response, languageType);
+
     }
 
     @Override

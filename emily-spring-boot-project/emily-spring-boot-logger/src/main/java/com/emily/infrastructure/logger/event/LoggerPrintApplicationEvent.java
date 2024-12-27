51fd44b1bc18569199f8aebbb3241d5a07cb1856
yaomingyang <mingyang@eastmoney.com>
2024-12-27 16:05:01 +0800

    LoggerPrintApplicationEvent 事件空值校验

--- emily-spring-boot-project/emily-spring-boot-logger/src/main/java/com/emily/infrastructure/logger/event/LoggerPrintApplicationEvent.java
+++ emily-spring-boot-project/emily-spring-boot-logger/src/main/java/com/emily/infrastructure/logger/event/LoggerPrintApplicationEvent.java
@@ -2,6 +2,7 @@ package com.emily.infrastructure.logger.event;
 
 import com.emily.infrastructure.logback.entity.BaseLogger;
 import org.springframework.context.ApplicationEvent;
+import org.springframework.util.Assert;
 
 /**
  * 打印日志事件
@@ -21,6 +22,8 @@ public class LoggerPrintApplicationEvent extends ApplicationEvent {
 
     public LoggerPrintApplicationEvent(EventType eventType, BaseLogger baseLogger) {
         super(baseLogger);
+        Assert.notNull(eventType, "eventType must not be null");
+        Assert.notNull(baseLogger, "baseLogger must not be null");
         this.eventType = eventType;
         this.baseLogger = baseLogger;
     }

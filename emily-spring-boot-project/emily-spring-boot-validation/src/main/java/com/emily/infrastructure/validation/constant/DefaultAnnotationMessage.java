ac203a8c0fe28f84543dbaac092aecacfdde280f
yaomingyang <mingyang@eastmoney.com>
2025-07-28 15:41:12 +0800

    del useless code

--- emily-spring-boot-project/emily-spring-boot-validation/src/main/java/com/emily/infrastructure/validation/constant/DefaultAnnotationMessage.java
+++
@@ -1,9 +0,0 @@
-package com.emily.infrastructure.validation.constant;
-
-/**
- * @author :  Emily
- * @since :  2024/10/29 下午4:38
- */
-public class DefaultAnnotationMessage {
-    public static final String ACCOUNT_CODE_MESSAGE = "非法账号";
-}

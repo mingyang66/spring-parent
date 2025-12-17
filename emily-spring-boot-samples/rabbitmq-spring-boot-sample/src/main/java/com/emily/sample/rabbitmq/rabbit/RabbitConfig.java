223473547a1435ecfa8ca00504e73d70c6f6e395
yaomingyang <mingyang@eastmoney.com>
2025-12-17 14:29:15 +0800

    refactor: optimize code

--- emily-spring-boot-samples/rabbitmq-spring-boot-sample/src/main/java/com/emily/sample/rabbitmq/rabbit/RabbitConfig.java
+++ emily-spring-boot-samples/rabbitmq-spring-boot-sample/src/main/java/com/emily/sample/rabbitmq/rabbit/RabbitConfig.java
@@ -13,12 +13,11 @@ import java.io.IOException;
  * @author Emily
  * @since Created in 2022/6/10 4:48 下午
  */
-
 @Configuration
 @DependsOn(value = {"rabbitListenerContainerFactory"})
 public class RabbitConfig {
 
-    @RabbitListener(queues = "emily.test.queue", containerFactory = "emilyRabbitListenerContainerFactory", ackMode = "MANUAL")
+    @RabbitListener(queues = "emily.test.queue", containerFactory = "emilyRabbitListenerContainerFactory")
     public void handler(Channel channel, Message message) throws IOException {
         long deliveryTag = message.getMessageProperties().getDeliveryTag();
         System.out.println("EMILY-" + new String(message.getBody()));
@@ -26,7 +25,7 @@ public class RabbitConfig {
 
     }
 
-    @RabbitListener(queues = "topic.test.queue", ackMode = "MANUAL")
+    @RabbitListener(queues = "topic.test.queue")
     public void handlerEmily(Channel channel, Message message) throws IOException {
         long deliveryTag = message.getMessageProperties().getDeliveryTag();
         System.out.println("TEST-" + new String(message.getBody()));

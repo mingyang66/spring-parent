eaf724c564f4911d64a0780281bb245f170b6be7
yaomingyang <mingyang@eastmoney.com>
2026-01-06 17:21:54 +0800

    fix: optimize rabbit sdk code

--- emily-spring-boot-project/emily-spring-boot-rabbitmq/src/main/java/com/emily/infrastructure/rabbitmq/annotation/EnableRabbit.java
+++ emily-spring-boot-project/emily-spring-boot-rabbitmq/src/main/java/com/emily/infrastructure/rabbitmq/annotation/EnableRabbit.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
- * @author :  姚明洋
+ * @author :  Emily
  * @since :  2025/12/25 下午3:17
  */
 @Target({ElementType.TYPE})

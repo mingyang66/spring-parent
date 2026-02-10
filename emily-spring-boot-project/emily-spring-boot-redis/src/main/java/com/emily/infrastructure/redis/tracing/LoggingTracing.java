5be5d7711ded6870f2cf998bebea97bc9dd8253f
yaomingyang <mingyang@eastmoney.com>
2026-02-10 13:31:19 +0800

    feat: optimize code

--- emily-spring-boot-project/emily-spring-boot-redis/src/main/java/com/emily/infrastructure/redis/tracing/LoggingTracing.java
+++ emily-spring-boot-project/emily-spring-boot-redis/src/main/java/com/emily/infrastructure/redis/tracing/LoggingTracing.java
@@ -11,20 +11,21 @@ import java.net.SocketAddress;
 
 public class LoggingTracing implements Tracing {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoggingTracing.class);
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingTracing.class);
 
     private final TracerProvider tracerProvider = new LoggingTracerProvider();
-    // 对于简单的日志追踪，我们提供一个不返回任何上下文的提供者，
-    // 让 Lettuce 知道当前没有活跃的追踪上下文，从而开始一个新的追踪。
+    /**
+     * 对于简单的日志追踪，我们提供一个不返回任何上下文的提供者，
+     * 让 Lettuce 知道当前没有活跃的追踪上下文，从而开始一个新的追踪。
+     */
     private final TraceContextProvider initialTraceContextProvider = new NoOpTraceContextProvider();
+    /**
+     * Tracer标签是否包含命令参数
+     */
     private final boolean includeCommandArgsInSpanTags;
 
     public LoggingTracing(boolean includeCommandArgsInSpanTags) {
         this.includeCommandArgsInSpanTags = includeCommandArgsInSpanTags;
-    }
-
-    public LoggingTracing() {
-        this(false);
     }
 
     @Override
@@ -49,34 +50,25 @@ public class LoggingTracing implements Tracing {
 
     @Override
     public Endpoint createEndpoint(SocketAddress socketAddress) {
-        if (socketAddress instanceof java.net.InetSocketAddress) {
-            java.net.InetSocketAddress inetAddr = (java.net.InetSocketAddress) socketAddress;
+        if (socketAddress instanceof java.net.InetSocketAddress inetAddr) {
             return new LoggingEndpoint(inetAddr.getHostString(), inetAddr.getPort());
         }
-        logger.info("Cannot create endpoint for unsupported SocketAddress type: {}", socketAddress.getClass().getName());
+        LOG.info("Cannot create endpoint for unsupported SocketAddress type: {}", socketAddress.getClass().getName());
         return new LoggingEndpoint("unknown", 0);
     }
 
-    // 简单的 Endpoint 实现
-    static class LoggingEndpoint implements Endpoint {
-        private final String host;
-        private final int port;
-
-        LoggingEndpoint(String host, int port) {
-            this.host = host;
-            this.port = port;
-        }
-
-        public String host() {
-            return host;
-        }
-
-        public int port() {
-            return port;
-        }
+    /**
+     * 简单的 Endpoint 实现
+     *
+     * @param host
+     * @param port
+     */
+    record LoggingEndpoint(String host, int port) implements Endpoint {
     }
 
-    // 一个不返回任何上下文的 TraceContextProvider 实现
+    /**
+     * 一个不返回任何上下文的 TraceContextProvider 实现
+     */
     static class NoOpTraceContextProvider implements TraceContextProvider {
         @Override
         public TraceContext getTraceContext() {
